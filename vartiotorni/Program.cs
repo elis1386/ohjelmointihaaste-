59308be4772f705aaf35f71fe34e8c0a459f5050
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-09-28 10:27:29 +0300

    add 4 exersise

---
+++ vartiotorni/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace exercise4
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjoitaa X arvo: ");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Kirjoitaa Y arvo: ");
+            int y = Int32.Parse(Console.ReadLine());
+
+            if(y > 0 && x > 0)
+            {
+                Console.WriteLine("The enemy is the north east!");
+            }
+            if(y > 0 && x < 0)
+            {
+               Console.WriteLine("The enemy is the north east!"); 
+            }
+            if(x = 0 && y > 0)
+            {
+                Console.WriteLine("The enemy is the north!");
+            }
+            if(y = 0 && x < 0)
+            {
+                Console.WriteLine("The enemy is the west!");
+            }
+            if(y < 0 && x < 0)
+            {
+                Console.WriteLine("The enemy is the south west!");
+            }
+            if(y < 0 && x = 0)
+            {
+                Console.WriteLine("The enemy is the south!");
+            }
+            if(y < 0 && x > 0)
+            {
+                Console.WriteLine("The enemy is the south east!");
+            }
+            if(y = 0 && x > 0)
+            {
+                Console.WriteLine("The enemy is the east!");
+            }
+             if(y > 0 && x > 0)
+            {
+                Console.WriteLine("The enemy is the south north east!");
+            }
+        }
+    }
+}
