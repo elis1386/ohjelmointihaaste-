25ff23625f3fc1c77956821a1262efbd0832e99e
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-10-03 10:31:43 +0300

    lihapiiraka

---
+++ lihapiiraka/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exercise9
+{
+    using System;
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Kuinka paljon rahaa lompakossa?");
+            int rahaa = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Kuinka paljon maksaa lihapiiraka?");
+            int lihapiiraka = Int32.Parse(Console.ReadLine());
+
+            if(rahaa > lihapiiraka)
+            {
+                int result = rahaa - lihapiiraka;
+                Console.WriteLine($"Ostit piiraka ja vaihtorahasi {result} euro");
+            }
+            else{
+                Console.WriteLine("Valitettovasti ei ole tarpeksii rahaa,syödä toisen kerran");
+            }
+        }
+    }
+}
